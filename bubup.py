de8d89e1da586a3a08cb1f51bb746ae3ec7d6878
Joe Doe <joe.doe@engineer.com>
2015-11-20 11:02:54 +0200

    navbar and basic sceleton

--- bubup.py
+++ bubup.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='src/view/pages',
+            static_folder='src/view/static')
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('vertical_tabs.html')
+
+
+if __name__ == '__main__':
+    app.run(threaded=True,
+            debug=False,
+            use_reloader=False,
+            host='0.0.0.0',
+            port=5001)
